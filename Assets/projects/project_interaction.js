b70eeef8474350247eb042b39e51b7e9471fbe1a
WenqingYin <39289444+WenqingYin@users.noreply.github.com>
2020-07-10 16:03:37 -0400

    add new project

--- Assets/projects/project_interaction.js
+++ Assets/projects/project_interaction.js
@@ -8,7 +8,10 @@ for (var i=0; i<contentBlock.length; i++){
 	var currBlock=contentBlock[i];
 	if (i%2!=0){
 		currBlock.style.backgroundColor="#F7F7F7";
-	}
+    }
+    else{
+        currBlock.style.backgroundColor="white";
+    }
 }
 
 
@@ -38,4 +41,21 @@ span.onclick = function() {
 }
 
 
+$(document).ready(function(){
 
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+
+    //Click event to scroll to top
+    $('.scrollToTop').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+
+});
